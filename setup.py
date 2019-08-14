222468d7337a5a19a38bad37510a8aaf4fd2c3c6
dradamski <drew.adamski@gmail.com>
2019-08-13 19:29:28 -0700

    add all files to repo

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A data science project to improve response to poor water quality conditions.',
+    author='Drew Adamski',
+    license='MIT',
+)
